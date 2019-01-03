b13ef69894d3303a150e595390b2e89b58b1f89d
kcinnick <nicktucker4@gmail.com>
2019-01-02 23:19:00 -0500

    Split up tests in test_posh.py

--- tests/test_posh.py
+++ tests/test_posh.py
@@ -41,6 +41,30 @@ def test_build_request():
         assert test_string == 'https://poshmark.com/brand/LuLaRoe-Women-D' +\
             'resses-Mini-color-Black-size-M?sort_by=add' + \
             'ed_desc&condition=closet&price%5B%5D=26-50'
+
+
+def test_execute_search():
+    possible_arguments = OrderedDict({
+        'brand': 'LuLaRoe',
+        'sex': 'Women',
+        'category': 'Dresses',
+        'subcategory': 'Mini',
+        'color': 'Black',
+        'size': 'M',
+        'sort': 'added_desc',
+        'type': 'closet',
+        'price': '26-50'
+    })
+
+    possible_arguments = dict(sorted(possible_arguments.items(),
+                                     key=lambda x: random()))
+    for i in range(1, 5):
+        possible_arguments = dict(sorted(possible_arguments.items(),
+                                         key=lambda x: random()))
+        test_string = product_search._build_request(possible_arguments)
+        assert test_string == 'https://poshmark.com/brand/LuLaRoe-Women-D' +\
+            'resses-Mini-color-Black-size-M?sort_by=add' + \
+            'ed_desc&condition=closet&price%5B%5D=26-50'
         product_search.execute_search(test_string, page_number=2)
         assert len(product_search.results) == 48
         product_search.results = []
