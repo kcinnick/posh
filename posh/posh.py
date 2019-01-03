f899b04cf17283141a21d7a918a61f9d09331f95
kcinnick <nicktucker4@gmail.com>
2019-01-02 19:06:45 -0500

    added execute_search method, Product class.

--- posh/posh.py
+++ posh/posh.py
@@ -1,8 +1,31 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+from bs4 import BeautifulSoup
 from collections import OrderedDict
 import requests
+
+
+class Product:
+    def __init__(self, url, posted_at=None, owner=None, brand=None,
+                 price=None, size=None, listing_id=None, title=None):
+        self.url = url
+        self.posted_at = posted_at
+        self.owner = owner
+        self.brand = brand
+        self.price = price
+        self.size = size
+        self.listing_id = listing_id
+        self.title = title
+
+    def _build_product_from_tile(self, tile):
+        self.posted_at = tile['data-created-at']
+        self.owner = tile['data-creator-handle']
+        self.brand = tile['data-post-brand']
+        self.price = tile['data-post-price']
+        self.size = tile['data-post-size']
+        self.listing_id = tile['id']
+        self.title = tile.find('a')['title']
 
 
 class ProductSearch:
@@ -18,6 +41,7 @@ class ProductSearch:
         self.subcategory = subcategory
         self.condition = condition
         self.availability = availability
+        self.results = []
 
     def set_headers(self, headers=None):
         if not headers:
@@ -46,4 +70,17 @@ class ProductSearch:
 
         return string
 
+    def execute_search(self, request_str, page_number=None, items=None):
+        if page_number:
+            request_str += f'&max_id={page_number}'
 
+        r = self.session.get(request_str)
+        soup = BeautifulSoup(r.content, 'lxml')
+        tiles = soup.find_all('div', class_='tile')
+        for tile in tiles:
+            p = Product(
+                url=f"https://poshmark.com{tile.find('a').get('href')}")
+            p._build_product_from_tile(tile)
+            self.results.append(p)
+
+        return
