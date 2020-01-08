e99cec003a52a01ef1be040d9cb4bf98bfacdcad
Nick Tucker <nicktucker4@gmail.com>
2020-01-08 17:53:56 -0500

    Update test_account.py

--- tests/test_account.py
+++ tests/test_account.py
@@ -12,7 +12,7 @@ from posh.product import Product, get_past_date
 product_search = ProductSearch()
 
 
-#@pytest.mark.skip(reason="Not always working re: cloudflare issues.")
+@pytest.mark.skip(reason="Not always working re: cloudflare issues.")
 def test_account_login():
     test_account = Account(username='ntucker12312', password='testing_for_posh1')
     with pytest.raises(LoginError):
@@ -21,7 +21,7 @@ def test_account_login():
     assert test_account.check_login()
 
 
-#@pytest.mark.skip(reason="Not always working re: cloudflare issues.")
+@pytest.mark.skip(reason="Not always working re: cloudflare issues.")
 def test_product_like():
     #  Because Poshmark uses ReCAPTCHA, this test will fail unless the user is
     #  already logged in to the Poshmark website.
@@ -35,7 +35,7 @@ def test_product_like():
     assert product.like(test_account)
 
 
-#@pytest.mark.skip(reason="Not always working re: cloudflare issues.")
+@pytest.mark.skip(reason="Not always working re: cloudflare issues.")
 def test_product_share():
     test_account = Account(username='ntucker12312', password='testing_for_posh1')
     test_account.login()
@@ -47,7 +47,7 @@ def test_product_share():
     assert product.share(test_account)
 
 
-#@pytest.mark.skip(reason="Not always working re: cloudflare issues.")
+@pytest.mark.skip(reason="Not always working re: cloudflare issues.")
 def test_product_comment():
     test_account = Account(username='ntucker12312', password='testing_for_posh1')
     test_account.login()
